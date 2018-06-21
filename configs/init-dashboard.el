c77e56372db538a78713a5256a7410288008e303
Kenton Wang <smartepsh@gmail.com>
2018-06-21 23:16:38 +0800

    feat: setup dashboard with xuenv

---
+++ configs/init-dashboard.el
@@ -0,0 +1,11 @@
+(use-package page-break-lines)
+
+(use-package dashboard
+   :init
+  (setq dashboard-banner-logo-title "Hi! Kenton Wang!")
+  (setq dashboard-startup-banner "~/.emacs.d/statics/xuenv.jpeg")
+  (turn-on-page-break-lines-mode)
+  :config
+  (dashboard-setup-startup-hook))
+
+(provide 'init-dashboard)
--- init.el
+++ init.el
@@ -4,6 +4,8 @@
 
 (require 'init-package)
 (require 'init-evil)
+(require 'init-dashboard)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
