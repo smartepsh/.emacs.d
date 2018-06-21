480f169d78d5f19083fbd7b32c0784fba56a7ebd
Kenton Wang <smartepsh@gmail.com>
2018-06-21 22:39:11 +0800

    feat: add use package

---
+++ configs/init-package.el
@@ -0,0 +1,17 @@
+(package-initialize)
+
+(setq package-enable-at-startup nil)
+(setq package-archives '(("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
+                        ("org-cn"   . "http://elpa.zilongshanren.com/org/")
+                        ("gnu-cn"   . "http://elpa.zilongshanren.com/gnu/")))
+(setq package-user-dir "~/.emacs.d/packages")
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
+
+(setq use-package-always-ensure t)
+
+(provide 'init-package)
--- init.el
+++ init.el
@@ -1,3 +1,17 @@
 (add-to-list 'load-path "~/.emacs.d/configs/")
 
 (setq private/cache-directory "~/.emacs.d/.cache/")
+
+(require 'init-package)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
