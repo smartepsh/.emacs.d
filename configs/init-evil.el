e4504e0a43ac5d54f5231d75bb63db0b98377447
Kenton Wang <smartepsh@gmail.com>
2018-06-22 21:58:30 +0800

    feat: add some evil configs

--- configs/init-evil.el
+++ configs/init-evil.el
@@ -1,5 +1,16 @@
 (use-package evil
+  :init
+  (setq evil-shift-width 2)
+  (setq evil-flash-delay 1)
+  (evil-mode 1)
   :config
-  (evil-mode 1))
+  (mapc #'evil-declare-ignore-repeat
+    '(kill-this-buffer
+    ido-kill-buffer
+    save-buffer
+    split-window-below
+    split-window-below-and-focus
+    split-window-right
+    split-window-right-and-focus)))
 
 (provide 'init-evil)
