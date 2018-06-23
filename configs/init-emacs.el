54e77e6871794ab17717ae161d63494c23674e5a
Kenton Wang <smartepsh@gmail.com>
2018-06-23 21:22:02 +0800

    feat: add restart emacs

---
+++ configs/init-emacs.el
@@ -0,0 +1,6 @@
+(use-package restart-emacs
+  :config
+  (global-set-key (kbd "H-r") 'restart-emacs)
+  )
+
+(provide 'init-emacs)
--- init.el
+++ init.el
@@ -14,6 +14,7 @@
 (setq create-lockfiles nil)
 (require 'init-package)
 (require 'init-server)
+(require 'init-emacs)
 (require 'init-ui)
 (require 'init-evil)
 (require 'init-chinese)
