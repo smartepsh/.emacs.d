840aba600c5329fbf4dccf296806e71acf842d02
Kenton Wang <smartepsh@gmail.com>
2018-08-14 17:15:41 +0800

    clear magit SPC keybindings

--- configs/init-magit.el
+++ configs/init-magit.el
@@ -1,6 +1,15 @@
 (use-package magit
   :commands (magit-status)
   :general
+  (clear-spc
+   :keymaps
+   `(magit-mode-map
+     magit-status-mode-map
+     magit-diff-mode-map
+     magit-process-mode-map
+     magit-blame-read-only-mode-map
+     magit-log-mode-map
+     git-rebase-mode-map))
   (my-leader-def
   "gs" 'magit-status
   ))
