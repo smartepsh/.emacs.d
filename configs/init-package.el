860266c536907b60d2b8dbecd662d1c55b1949df
Kenton Wang <smartepsh@gmail.com>
2018-08-14 17:38:28 +0800

    remove update package automaticly

--- configs/init-package.el
+++ configs/init-package.el
@@ -16,7 +16,7 @@
 (use-package auto-package-update
   :ensure t
   :config
-  (setq auto-package-update-delete-old-versions nil)
-  (auot-package-update-maybse))
+  (setq auto-package-update-delete-old-versions nil))
+  
 
 (provide 'init-package)
