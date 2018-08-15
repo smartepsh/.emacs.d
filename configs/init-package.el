e417990e1b691755ef20612e4f04516be77754a3
Kenton Wang <smartepsh@gmail.com>
2018-08-15 14:47:57 +0800

    use quelpa

--- configs/init-package.el
+++ configs/init-package.el
@@ -1,25 +1,42 @@
 (package-initialize)
 
-(setq package-enable-at-startup nil)
-(setq package-archives '(("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
+;; (unless (package-installed-p 'use-package)
+;;   (package-refresh-contents)
+;;   (package-install 'use-package))
+
+;; disable update during quelpa initialized
+(setq quelpa-update-melpa-p nil)
+(setq quelpa-checkout-melpa-p nil)
+;; install quelpa
+(unless (require 'quelpa nil t)
+  (with-temp-buffer
+    (url-insert-file-contents "https://framagit.org/steckerhalter/quelpa/raw/master/bootstrap.el")
+    (eval-buffer)))
+
+;; (quelpa-self-upgrade)
+
+;; install quelpa-use-package
+(quelpa
+ '(quelpa-use-package
+   :fetcher git
+   :url "https://framagit.org/steckerhalter/quelpa-use-package.git"))
+(require 'quelpa-use-package)
+
+;; (require 'use-package)
+
+;; (setq use-package-ensure-function 'quelpa)
+(setq use-package-expand-minimally t
+      use-package-always-ensure t
+      package-enable-at-startup nil
+      package-archives '(("melpa-cn" . "http://elpa.zilongshanren.com/melpa/")
                         ("org-cn"   . "http://elpa.zilongshanren.com/org/")
                         ("gnu-cn"   . "http://elpa.zilongshanren.com/gnu/")))
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+(quelpa-use-package-activate-advice)
 
-(require 'use-package)
-
-(setq use-package-always-ensure t)
 
 (use-package auto-package-update
-  :ensure t
   :config
   (setq auto-package-update-delete-old-versions nil))
-
-(use-package quelpa-use-package
-  :after use-package)
-  
 
 (provide 'init-package)
