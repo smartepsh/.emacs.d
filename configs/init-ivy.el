9d64b86cc6e7fa80e2327249dc9e696da455bacd
Kenton Wang <smartepsh@gmail.com>
2018-08-15 15:55:53 +0800

    add ag key binding

--- configs/init-ivy.el
+++ configs/init-ivy.el
@@ -4,7 +4,10 @@
   :config
   (setq ivy-use-virtual-buffers t)
   (setq enable-recursive-minibuffers t)
-  (global-set-key "\C-s" 'swiper))
+  (global-set-key "\C-s" 'swiper)
+  :general
+  (common-leader
+    "/" 'counsel-ag))
 
 (use-package swiper
   :after ivy)
