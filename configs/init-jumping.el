21e1eb27bd48baba3261f2a2ab25ca92c3095840
Kenton Wang <smartepsh@gmail.com>
2018-08-16 14:16:49 +0800

    move vimish fold

--- configs/init-jumping.el
+++ configs/init-jumping.el
@@ -8,4 +8,21 @@
     "jj" 'avy-goto-char-2
     "jl" 'avy-goto-line))
 
+(use-package vimish-fold
+  :config
+  (vimish-fold-global-mode t)
+  :general
+  (common-leader
+    :keymaps 'vimish-fold-mode-map
+    "fc" `vimish-fold
+    "fu" `vimish-fold-unfold
+    "fj" `vimish-fold-avy
+    "fz" `vimish-fold-toggle
+    "fd" `vimish-fold-delete
+    "fad" `vimish-fold-delete-all
+    "fau" `vimish-fold-unfold-all
+    "far" `vimish-fold-refold-all
+    "fat" `vimish-fold-toggle-all))
+
+
 (provide 'init-jumping)
