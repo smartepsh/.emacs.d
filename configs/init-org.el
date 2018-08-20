f2ccb57fc6236df4c52a7989ff58d828392a2ff8
Kenton Wang <smartepsh@gmail.com>
2018-08-20 15:28:06 +0800

    add org mac link

--- configs/init-org.el
+++ configs/init-org.el
@@ -1,5 +1,19 @@
 (use-package org
+  :ensure org-plus-contrib
+  :pin org-cn
   :defer t
   )
+
+(use-package org-mac-link
+  :ensure org-plus-contrib
+  :commands (org-mac-skim-insert-page
+	     org-mac-chrome-insert-frontmost-url
+	     org-mac-finder-insert-selected)
+  :general
+  (local-leader
+    :keymaps 'org-mode-map
+    "cc" 'org-mac-chrome-insert-frontmost-url
+    "ck" 'org-mac-skim-insert-page
+    "cf" 'org-mac-finder-insert-selected))
 
 (provide 'init-org)
