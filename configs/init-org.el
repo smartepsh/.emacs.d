d480bd573ab2f0375d227e65f39f7b87a83a1c39
Kenton Wang <smartepsh@gmail.com>
2018-08-21 16:21:02 +0800

    add epub reader - nov and org-noter for it

--- configs/init-org.el
+++ configs/init-org.el
@@ -2,6 +2,9 @@
   :ensure org-plus-contrib
   :pin org-cn
   :defer t
+  :general
+  (common-leader
+    "on" 'org-noter)
   )
 
 (use-package org-mac-link
@@ -21,5 +24,44 @@
   :after org
   :init
   (add-to-list 'org-babel-load-languages '(elixir . t)))
+
+(use-package nov
+  :mode ("\\.epub\\'" . nov-mode)
+  :general
+  ;; disable special "h" key action
+  (general-define-key
+   :keymaps '(nov-mode-map)
+   "h" nil) 
+  (clear-spc :keymaps '(nov-mode-map))
+  (local-leader
+    :keymaps 'nov-mode-map
+    "n" 'nov-next-document
+    "p" 'nov-previous-document
+    "m" 'nov-goto-toc
+    "," 'nov-brower-url
+    :keymaps 'org-noter-doc-mode-map
+    "i" 'org-noter-insert-note
+    "I" 'org-noter-insert-precise-note
+    "sC" 'org-noter-sync-current-page-or-chapter
+    "sc" 'org-noter-sync-current-note
+    "sN" 'org-noter-sync-next-page-or-chapter
+    "sn" 'org-noter-sync-next-note
+    "sP" 'org-noter-sync-prev-page-or-chapter
+    "sp" 'org-noter-sync-prev-note))
+
+(use-package org-noter
+  :commands org-noter
+  :hook (org-mode . org-noter-notes-mode)
+  :init
+  (setq org-noter-always-create-frame nil)
+  :general
+  (local-leader
+    :keymaps 'org-noter-notes-mode-map
+    "sC" 'org-noter-sync-current-page-or-chapter
+    "sc" 'org-noter-sync-current-note
+    "sN" 'org-noter-sync-next-page-or-chapter
+    "sn" 'org-noter-sync-next-note
+    "sP" 'org-noter-sync-prev-page-or-chapter
+    "sp" 'org-noter-sync-prev-note))
 
 (provide 'init-org)
