0c2c9d5b78f00fd2e7bda456ca24eb86eb087f8b
Kenton Wang <smartepsh@gmail.com>
2018-08-21 19:03:31 +0800

    setup company mode

--- configs/init-company.el
+++ configs/init-company.el
@@ -2,23 +2,39 @@
   :defer t
   :init
   (global-company-mode)
-  :config
+  (setq company-idle-delay nil
+	company-require-match 'never
+	company-tooltip-align-annotations t
+	company-dabbrev-ignore-case nil
+	company-dabbrev-downcase nil)
+
   (setq company-backends
 	'(company-keywords
 	  company-files
 	  company-caps
 	  company-dabbrev-code
-	  company-dabbrev)))
+	  company-dabbrev))
+  :config
+  (defun +evil-complete (arg)
+    (interactive)
+    (unless (company-complete)
+      (hippie-expand arg)))
+
+  (setq evil-complete-next-func '+evil-complete
+	evil-complete-previous-func '+evil-complete)
+
+  :general
+  (general-define-key
+   :keymaps 'company-active-map
+   "C-w" nil
+   "C-n" 'company-select-next
+   "C-p" 'company-select-previous))
 
 (use-package company-statistics
-  :defer t
-  :init
-  (add-hook 'company-mode-hook 'company-statistics-mode))
+  :hook (company-mode . company-statistics-mode))
 
 (use-package smartparens
-  :defer t
-  :init
-  (smartparens-global-mode)
+  :hook (prog-mode . smartparens-mode)
   :config
   (show-smartparens-mode))
 
