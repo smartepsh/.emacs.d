eb9539541b77c9a5bc05957638ad54efa4158590
Kenton Wang <smartepsh@gmail.com>
2018-08-21 18:13:50 +0800

    set dired

--- configs/init-file-nav.el
+++ configs/init-file-nav.el
@@ -5,13 +5,24 @@
 
 (use-package dired
   :ensure nil
+  :init
+  (setq dired-dwim-target t
+	dired-listing-switches "-alh")
+  :config
+  (add-to-list 'evil-normal-state-modes 'wdired-mode)
   :general
+  (general-nmap
+   :keymaps 'dired-mode-map
+   "w" 'wdired-change-to-wdired-mode)
   (clear-spc
     :keymaps 'dired-mode-map)
   (common-leader
     "fd" 'dired-jump
     "fD" 'dired-jump-other-window))
 
-(global-set-key [f5] 'neotree-toggle)
+(use-package dired-k
+  :hook dired-mode
+  :config
+  (dired-k))
 
 (provide 'init-file-nav)
