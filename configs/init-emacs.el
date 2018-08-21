a8e925b5cc217e2d66c63bb143254d687d937c9f
Kenton Wang <smartepsh@gmail.com>
2018-08-21 17:15:48 +0800

    add SPC f e d like spacemacs to open config file

--- configs/init-emacs.el
+++ configs/init-emacs.el
@@ -1,8 +1,14 @@
 (use-package restart-emacs
   :bind ("H-r" . 'restart-emacs))
 
+(defun open-config-file ()
+  "open emacs configuration file"
+  (interactive)
+  (find-file (expand-file-name "~/.emacs.d/init.el")))
+
 (common-leader
   "SPC" 'execute-extended-command
+  "fed" 'open-config-file
   "ff" 'find-file)
 
 ;; (vimish-fold--vimish-overlay-p)
