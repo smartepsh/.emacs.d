a1b6e41ccacbcfde96134941e6592b6a2c4a0df3
Kenton Wang <smartepsh@gmail.com>
2018-08-22 18:52:24 +0800

    spell check

--- configs/init-spell-checking.el
+++ configs/init-spell-checking.el
@@ -1,13 +1,23 @@
-(use-package ispell
-  :defer t)
-
 (use-package flyspell
-  :defer t)
+  :ensure-system-package hunspell
+  :hook (text-mode . flyspell-mode)
+  :hook (prog-mode . flyspell-prog-mode)
+  :init
+  (setq ispell-program-name (executable-find "hunspell")
+	ispell-dictionary "en_US"
+	ispell-local-dictionary-alist '(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil ("-d" "en_US,en_US-med") nil utf-8))))
 
 (use-package flyspell-correct-ivy
-  :commands
-  (flyspell-correct-ivy)
+  :after flyspell-correct)
+
+(use-package flyspell-correct
+  :commands (flyspell-correct-word-generic
+	     flyspell-correct-previous-word-generic)
   :init
-  (setq flyspell-correct-interface #'flyspell-correct-ivy))
+  (setq flyspell-correct-interface 'flyspell-correct-ivy)
+  :general
+  (general-define-key
+   :keymaps 'flyspell-mode-map
+   "C-;" 'flyspell-correct-previous-word-generic))
 
 (provide 'init-spell-checking)
