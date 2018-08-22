1e48e2040b303f4a9db3231a1b02ccdbfdd9d74b
Kenton Wang <smartepsh@gmail.com>
2018-08-22 11:38:00 +0800

    fix dired ls does not support dired error

--- configs/init-file-nav.el
+++ configs/init-file-nav.el
@@ -8,7 +8,9 @@
   :commands (dired-jump dired-jump-other-window)
   :init
   (setq dired-dwim-target t
-	dired-listing-switches "-alh")
+	dired-listing-switches "-alh"
+	insert-directory-program "gls"
+	dired-use-ls-dired t)
   :config
   (add-to-list 'evil-normal-state-modes 'wdired-mode)
   :general
@@ -20,6 +22,9 @@
   (common-leader
     "fd" 'dired-jump
     "fD" 'dired-jump-other-window))
+
+(use-package dired-quick-sort
+  :hook (dired-mode . dired-quick-sort-setup))
 
 (use-package dired-k
   :hook (dired-mode . dired-k))
