9819df2ec850a06a639a1c1b90a7fc53e2055742
Kenton Wang <smartepsh@gmail.com>
2018-08-22 13:06:40 +0800

    add mix format before save

--- configs/init-elixir.el
+++ configs/init-elixir.el
@@ -1,9 +1,21 @@
 (use-package elixir-mode
-             :defer t)
+  :defer t
+  :config
+  (add-hook 'elixir-mode-hook
+	    (lambda() (add-hook 'before-save-hook 'elixir-format nil t)))
+
+  (defun +reset-mixfmt-args ()
+    (let* ((formatter-directory (locate-dominating-file default-directory ".formatter.exs"))
+	   (formatter-file (concat formatter-directory ".formatter.exs")))
+      (if formatter-directory
+	  (setq elixir-format-arguments (list "--dot-formatter" formatter-file))
+	(setq elixir-format-arguments nil))))
+
+  (add-hook 'elixir-format-hook #'+reset-mixfmt-args))
 
 (use-package alchemist
-             :defer t
-             :init
-             (add-hook 'elixir-mode-hook 'alchemist-mode))
+  :defer t
+  :init
+  (add-hook 'elixir-mode-hook 'alchemist-mode))
 
 (provide 'init-elixir)
