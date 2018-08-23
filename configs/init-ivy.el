236376b425b5974be20c3c54c272998f1485c3e6
Kenton Wang <smartepsh@gmail.com>
2018-08-23 12:02:13 +0800

    add ivy and wgrep packages

--- configs/init-ivy.el
+++ configs/init-ivy.el
@@ -5,7 +5,17 @@
   (add-hook 'after-init-hook #'ivy-mode)
   (setq ivy-use-virtual-buffers t
         enable-recursive-minibuffers t)
+  :config
+  (add-to-list 'evil-normal-state-modes 'ivy-occur-grep-mode)
   :general
+  (general-define-key
+   :keymaps 'ivy-minibuffer-map
+   [escape] 'minibuffer-keyboard-quit)
+  (clear-spc
+    :keymaps 'ivy-occur-grep-mode-map)
+  (local-leader
+    :keymaps 'ivy-occur-grep-mode-map
+    "w" 'ivy-wgrep-change-to-wgrep-mode)
   (common-leader
     "/" 'counsel-ag))
 
@@ -17,6 +27,14 @@
 
 (use-package counsel
   :after ivy)
+
+(use-package ivy-rich
+  :after ivy
+  :init
+  (setq ivy-virtual-abbreviate 'full
+	ivy-rich-switch-buffer-align-virtual-buffer t)
+  :config
+  (ivy-set-display-transformer 'ivy-switch-buffer 'ivy-rich-switch-buffer-transaformer))
 
 (use-package ivy-prescient
   :after ivy
--- configs/init-support.el
+++ configs/init-support.el
@@ -4,4 +4,9 @@
   (setq prescient-filter-method 'regexp)
   (prescient-persist-mode))
 
+(use-package wgrep
+  :after (:any ivy)
+  :config
+  (setq wgrep-auto-save-buffer t))
+
 (provide 'init-support)
