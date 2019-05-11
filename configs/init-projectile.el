b0e8fd02136e508733fadf5889588e11f5567a91
Kenton <smartepsh@gmail.com>
2019-05-12 01:14:44 +0800

    feat: discover all projects under projectile search path

--- configs/init-projectile.el
+++ configs/init-projectile.el
@@ -1,15 +1,18 @@
 (use-package projectile
   :commands (projectile-dired)
   :init
-  (setq projectile-sort-order 'recentf
+  (setq ;;projectile-sort-order 'recentf-active
 	project-cache-file "~/.emacs.d/projectile.cache"
 	project-known-projects-file "~/.emacs.d/projectile-bookmarks.eld"
+        projectile-project-search-path '("~/kenton/")
 	projectile-completion-system 'ivy)
   :config
   (projectile-mode t)
+  (projectile-discover-projects-in-search-path)
   :general
   (common-leader
     "p" '(:ignore t :which-key "projectile")
+    "pa" '(projectile-discover-projects-in-search-path :which-key "find-all-projects")
     "pc" 'projectile-kill-buffers
     "pd" 'projectile-dired
     "pI" 'projectile-invalidate-cache))
