f5fb1e56f2731d62c745e430c9a0a14e2c0e067e
Kenton <smartepsh@gmail.com>
2019-12-08 18:28:53 +0800

    delete lsp code for debug init

--- configs/init-lsp.el
+++ configs/init-lsp.el
@@ -1,33 +1,33 @@
-(use-package lsp-mode
-  :hook (elixir-mode . lsp)
-  :commands lsp
-  :ensure-system-package (tsc . "npm install -g typescript")
-  :ensure-system-package (typescript-language-server . "npm install -g typescript-language-server")
-  :ensure-system-package (vls . "npm install -g vue-language-server")
-  :init
-  (add-to-list 'exec-path "~/.emacs.d/lsp/")
-  (add-to-list 'company-backends 'company-lsp)
-  :config
-  (setq lsp-file-watch-threshold nil))
-
-(use-package lsp-ui
-  :after (lsp-mode)
-  :hook (lsp-mode . lsp-ui-mode))
-
-(use-package company-lsp
-  :after (company, lsp-mode)
-  :init
-  (setq company-lsp-enable-snippet t)
-  (push 'company-lsp company-backends))
-
-(use-package dap-mode
-  :defer t
-  :init
-  (add-hook 'lsp 'dap-mode)
-  (add-hook 'lsp 'dap-ui-mode)
-  :config
-  (dap-tootip-mode t)
-  (tootip-mode t))
-
-
-(provide 'init-lsp)
+;; (use-package lsp-mode
+;;   :hook (elixir-mode . lsp)
+;;   :commands lsp
+;;   :ensure-system-package (tsc . "npm install -g typescript")
+;;   :ensure-system-package (typescript-language-server . "npm install -g typescript-language-server")
+;;   :ensure-system-package (vls . "npm install -g vue-language-server")
+;;   :init
+;;   (add-to-list 'exec-path "~/.emacs.d/lsp/")
+;;   (add-to-list 'company-backends 'company-lsp)
+;;   :config
+;;   (setq lsp-file-watch-threshold nil))
+;;
+;; (use-package lsp-ui
+;;   :after (lsp-mode)
+;;   :hook (lsp-mode . lsp-ui-mode))
+;;
+;; (use-package company-lsp
+;;   :after (company, lsp-mode)
+;;   :init
+;;   (setq company-lsp-enable-snippet t)
+;;   (push 'company-lsp company-backends))
+;;
+;; (use-package dap-mode
+;;   :defer t
+;;   :init
+;;   (add-hook 'lsp 'dap-mode)
+;;   (add-hook 'lsp 'dap-ui-mode)
+;;   :config
+;;   (dap-tootip-mode t)
+;;   (tootip-mode t))
+;;
+;;
+;; (provide 'init-lsp)
