a457f6e570af8d5d1ff4c5b35bb1c3dde64c5fb7
Kenton <smartepsh@gmail.com>
2020-03-12 14:10:34 +0800

    refactor: init-lsp

--- configs/init-lsp.el
+++ configs/init-lsp.el
@@ -1,15 +1,13 @@
 (use-package lsp-mode
   :hook (c-mode . lsp)
-  :hook (elixir-mode . lsp)
+  ;; :hook (elixir-mode . lsp)
   :commands lsp
-  ;; :ensure-system-package (tsc . "npm install -g typescript")
-  ;; :ensure-system-package (typescript-language-server . "npm install -g typescript-language-server")
-  ;; :ensure-system-package (vls . "npm install -g vue-language-server")
   :init
-  (add-to-list 'exec-path "~/.emacs.d/lsp/")
+  ;; (add-to-list 'exec-path "~/.emacs.d/lsp/")
   (add-to-list 'company-backends 'company-lsp)
   (setq lsp-keymap-prefix "s-l")
   :config
+  (lsp-enable-which-key-integration)
   (setq lsp-file-watch-threshold nil))
 
 (use-package lsp-ui
@@ -23,6 +21,9 @@
 (use-package lsp-ivy
   :commands lsp-ivy-workspace-symbol)
 
+(use-package lsp-treemacs
+  :commands lsp-treemacs-errors-list)
+
 ;; (use-package dap-mode
 ;;   :init
 ;;   (add-hook 'lsp 'dap-mode)
@@ -32,4 +33,7 @@
 ;;   (dap-tooltip-mode t)
 ;;   (tooltip-mode t))
 
+  ;; :ensure-system-package (tsc . "npm install -g typescript")
+  ;; :ensure-system-package (typescript-language-server . "npm install -g typescript-language-server")
+  ;; :ensure-system-package (vls . "npm install -g vue-language-server")
 (provide 'init-lsp)
