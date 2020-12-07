84c1caf3281136b2dfb094e6d5d64d9db5c8f02b
Kenton <smartepsh@gmail.com>
2020-12-08 01:14:59 +0800

    feat: use nox and exunit to replace alchemist

--- configs/init-elixir.el
+++ configs/init-elixir.el
@@ -11,124 +11,151 @@
 	  (setq elixir-format-arguments (list "--dot-formatter" formatter-file))
 	(setq elixir-format-arguments nil))))
 
-  (add-hook 'elixir-format-hook #'+reset-mixfmt-args))
+  (add-hook 'elixir-format-hook #'+reset-mixfmt-args)
 
-(use-package alchemist
-  :defer t
-  :init
-  (add-hook 'elixir-mode-hook 'alchemist-mode)
-  :config
-  (defun run-elixir-tests ()
-    (interactive)
-    (if (string-equal "exs" (file-name-extension (buffer-file-name (current-buffer))))
-    (alchemist-mix-test-this-buffer)
-    (alchemist-project-run-tests-for-current-file)))
-
-  (setq alchemist-test-status-modeline nil
-	alchemist-test-display-compilation-output t
-	alchemist-test-truncate-lines nil)
-	;; alchemist-goto-elixir-source-dir "~/kenton/elixir"
-	;; alchemist-goto-elixir-source-dir "/usr/local/Cellar/elixir/1.7.2")
-	;; alchemist-goto-erlang-source-dir "~/kenton/erlang")
   :general
   (local-leader
-    :keymaps 'alchemist-mode-map
-    "." '(alchemist-goto-definition-at-point :which-key "jump-to")
-    "," '(alchemist-goto-jump-back :which-key "jump-back")
+    :keymaps 'elixir-mode-map
+    "d" '(nox-show-doc :which-key "show doc")
+    "." '(xref-find-definitions :which-key "jump-to")
+    "C-." '(xref-find-definitions-other-window :which-key "jump-to-in-other-window")
+    "," '(xref-pop-marker-stack :which-key "jump-back")
+    "C-," '(xref-find-references :which-key "list references")))
 
+(use-package exunit
+  :defer t
+  :after elixir-mode
+  :commands (exunit-verify-all
+             exunit-verify
+             exunit-verify-all-in-umbrella
+             exunit-verify-single
+             exunit-rerun
+             exunit-toggle-file-and-test)
+  :general
+  (local-leader
     "t" '(:ignore t :which-key "test")
-    "ta" 'alchemist-mix-test
-    "tt" 'alchemist-mix-test-at-point
-    "tb" 'run-elixir-tests
-    "tr" 'alchemist-mix-rerun-last-test
-    "ts" 'alchemist-mix-test-stale
-    "tw" 'alchemist-test-toggle-test-report-display
-    "tf" 'alchemist-project-toggle-file-and-tests
+    "ta" 'exunit-verify-all
+    "tt" 'exunit-verify-single
+    "tb" 'exunit-verify
+    "tr" 'exunit-rerun
+    "tf" 'exunit-toggle-file-and-test
+    ))
+;; (use-package alchemist
+;;   :defer t
+;;   :init
+;;   (add-hook 'elixir-mode-hook 'alchemist-mode)
+;;   :config
+;;   (defun run-elixir-tests ()
+;;     (interactive)
+;;     (if (string-equal "exs" (file-name-extension (buffer-file-name (current-buffer))))
+;;     (alchemist-mix-test-this-buffer)
+;;     (alchemist-project-run-tests-for-current-file)))
 
-    "m" '(:ignore t :which-key "mix")
-    "m:" 'alchemist-mix-run
-    "mx" 'alchemist-mix
-    "mc" 'alchemist-mix-compile
-    "mr" 'alchemist-mix-rerun-last-task
+;;   (setq alchemist-test-status-modeline nil
+;; 	alchemist-test-display-compilation-output t
+;; 	alchemist-test-truncate-lines nil)
+;; 	;; alchemist-goto-elixir-source-dir "~/kenton/elixir"
+;; 	;; alchemist-goto-elixir-source-dir "/usr/local/Cellar/elixir/1.7.2")
+;; 	;; alchemist-goto-erlang-source-dir "~/kenton/erlang")
+;;   :general
+;;   (local-leader
+;;     :keymaps 'alchemist-mode-map
+;;     "." '(alchemist-goto-definition-at-point :which-key "jump-to")
+;;     "," '(alchemist-goto-jump-back :which-key "jump-back")
 
-    "h" '(:ignore t :which-key "hex")
-    "hi" 'alchemist-hex-info-at-point
-    "hr" 'alchemist-hex-releases-at-point
-    "hI" 'alchemist-hex-info
-    "hR" 'alchemist-hex-releases
-    "hs" 'alchemist-hex-search
-    "hd" 'alchemist-hex-all-dependences
+;;     "t" '(:ignore t :which-key "test")
+;;     "ta" 'alchemist-mix-test
+;;     "tt" 'alchemist-mix-test-at-point
+;;     "tb" 'run-elixir-tests
+;;     "tr" 'alchemist-mix-rerun-last-test
+;;     "ts" 'alchemist-mix-test-stale
+;;     "tw" 'alchemist-test-toggle-test-report-display
+;;     "tf" 'alchemist-project-toggle-file-and-tests
 
-    "c" '(:ignore t :which-key "compile")
-    "cb" 'alchemist-compile-this-buffer
-    "cc" 'alchemist-compile
+;;     "m" '(:ignore t :which-key "mix")
+;;     "m:" 'alchemist-mix-run
+;;     "mx" 'alchemist-mix
+;;     "mc" 'alchemist-mix-compile
+;;     "mr" 'alchemist-mix-rerun-last-task
 
-    "p" '(:ignore t :which-key "project")
-    "ps" 'alchemist-project-toggle-file-and-tests
-    "po" 'alchemist-project-toggle-file-and-tests-other-window
-    "pt" 'alchemist-project-run-tests-for-current-file
-    "pf" 'alchemist-project-find-test
-    "pl" 'alchemist-project-find-lib
+;;     "h" '(:ignore t :which-key "hex")
+;;     "hi" 'alchemist-hex-info-at-point
+;;     "hr" 'alchemist-hex-releases-at-point
+;;     "hI" 'alchemist-hex-info
+;;     "hR" 'alchemist-hex-releases
+;;     "hs" 'alchemist-hex-search
+;;     "hd" 'alchemist-hex-all-dependences
 
-    "f" '(:ignore t :which-key "phoenix-find")
-    "fw" 'alchemist-phoenix-find-web
-    "fc" 'alchemist-phoenix-find-controllers
-    "fl" 'alchemist-phoenix-find-channels
-    "ft" 'alchemist-phoenix-find-templates
-    "fm" 'alchemist-phoenix-find-models
-    "fv" 'alchemist-phoenix-find-views
-    "fs" 'alchemist-phoenix-find-static
-    "fr" 'alchemist-phoenix-router
-    "fR" 'alchemist-phoenix-routes
+;;     "c" '(:ignore t :which-key "compile")
+;;     "cb" 'alchemist-compile-this-buffer
+;;     "cc" 'alchemist-compile
 
-    "h" '(:ignore t :which-key "help")
-    "hh" 'alchemist-help
-    "hi" 'alchemist-help-history
-    "he" 'alchemist-help-search-at-point
-    "hr" 'alchemist-refcard
+;;     "p" '(:ignore t :which-key "project")
+;;     "ps" 'alchemist-project-toggle-file-and-tests
+;;     "po" 'alchemist-project-toggle-file-and-tests-other-window
+;;     "pt" 'alchemist-project-run-tests-for-current-file
+;;     "pf" 'alchemist-project-find-test
+;;     "pl" 'alchemist-project-find-lib
 
-    "g" '(:ignore t :which-key "goto")
-    "gg" 'alchemist-goto-definition-at-point
-    "gb" 'alchemist-goto-definition-at-point
-    "gp" 'alchemist-jump-to-previous-def-symbol
-    "gn" 'alchemist-jump-to-next-def-symbol
-    "gs" 'alchemist-goto-list-symbol-definitions'
+;;     "f" '(:ignore t :which-key "phoenix-find")
+;;     "fw" 'alchemist-phoenix-find-web
+;;     "fc" 'alchemist-phoenix-find-controllers
+;;     "fl" 'alchemist-phoenix-find-channels
+;;     "ft" 'alchemist-phoenix-find-templates
+;;     "fm" 'alchemist-phoenix-find-models
+;;     "fv" 'alchemist-phoenix-find-views
+;;     "fs" 'alchemist-phoenix-find-static
+;;     "fr" 'alchemist-phoenix-router
+;;     "fR" 'alchemist-phoenix-routes
 
-    "i" '(:ignore t :which-key "iex")
-    "ii" 'alchemist-iex-run
-    "ip" 'alchemist-iex-project-run
-    "il" 'alchemist-iex-send-current-line
-    "ic" 'alchemist-iex-send-current-line-and-go
-    "ir" 'alchemist-iex-send-region
-    "im" 'alchemist-iex-send-region-and-go
-    "ib" 'alchemist-iex-compile-this-buffer
-    "iR" 'alchemist-iex-reload-module
+;;     "h" '(:ignore t :which-key "help")
+;;     "hh" 'alchemist-help
+;;     "hi" 'alchemist-help-history
+;;     "he" 'alchemist-help-search-at-point
+;;     "hr" 'alchemist-refcard
 
-    "e" '(:ignore :which-key "eval")
-    "el" 'alchemist-eval-current-line
-    "ek" 'alchemist-eval-print-current-line
-    "ej" 'alchemist-eval-quoted-current-line
-    "eh" 'alchemist-eval-print-quoted-current-line
-    "eo" 'alchemist-eval-eval-region
-    "ei" 'alchemist-eval-print-region
-    "eu" 'alchemist-eval-quotaed-region
-    "ey" 'alchemist-eval-print-quoted-region
-    "eq" 'alchemist-eval-buffer
-    "ew" 'alchemist-eval-print-buffer
-    "ee" 'alchemist-eval-quoted-buffer
-    "er" 'alchemist-eval-print-quoted-buffer
-    "e!" 'alchemist-eval-close-popup
+;;     "g" '(:ignore t :which-key "goto")
+;;     "gg" 'alchemist-goto-definition-at-point
+;;     "gb" 'alchemist-goto-definition-at-point
+;;     "gp" 'alchemist-jump-to-previous-def-symbol
+;;     "gn" 'alchemist-jump-to-next-def-symbol
+;;     "gs" 'alchemist-goto-list-symbol-definitions'
 
-    "o" '(:ignore t :which-key "macro")
-    "ol" 'alchemist-macroexpand-once-current-line
-    "oL" 'alchemist-macroexpand-once-print-current-line
-    "ok" 'alchemist-macroexpand-current-line
-    "oK" 'alchemist-macroexpand-print-current-line
-    "oi" 'alchemist-macroexpand-once-region
-    "oI" 'alchemist-macroexpand-once-print-region
-    "or" 'alchemist-macroexpand-region
-    "oR" 'alchemist-macroexpand-print-region
-    "o!" 'alchemist-macroexpand-close-popup))
+;;     "i" '(:ignore t :which-key "iex")
+;;     "ii" 'alchemist-iex-run
+;;     "ip" 'alchemist-iex-project-run
+;;     "il" 'alchemist-iex-send-current-line
+;;     "ic" 'alchemist-iex-send-current-line-and-go
+;;     "ir" 'alchemist-iex-send-region
+;;     "im" 'alchemist-iex-send-region-and-go
+;;     "ib" 'alchemist-iex-compile-this-buffer
+;;     "iR" 'alchemist-iex-reload-module
+
+;;     "e" '(:ignore :which-key "eval")
+;;     "el" 'alchemist-eval-current-line
+;;     "ek" 'alchemist-eval-print-current-line
+;;     "ej" 'alchemist-eval-quoted-current-line
+;;     "eh" 'alchemist-eval-print-quoted-current-line
+;;     "eo" 'alchemist-eval-eval-region
+;;     "ei" 'alchemist-eval-print-region
+;;     "eu" 'alchemist-eval-quotaed-region
+;;     "ey" 'alchemist-eval-print-quoted-region
+;;     "eq" 'alchemist-eval-buffer
+;;     "ew" 'alchemist-eval-print-buffer
+;;     "ee" 'alchemist-eval-quoted-buffer
+;;     "er" 'alchemist-eval-print-quoted-buffer
+;;     "e!" 'alchemist-eval-close-popup
+
+;;     "o" '(:ignore t :which-key "macro")
+;;     "ol" 'alchemist-macroexpand-once-current-line
+;;     "oL" 'alchemist-macroexpand-once-print-current-line
+;;     "ok" 'alchemist-macroexpand-current-line
+;;     "oK" 'alchemist-macroexpand-print-current-line
+;;     "oi" 'alchemist-macroexpand-once-region
+;;     "oI" 'alchemist-macroexpand-once-print-region
+;;     "or" 'alchemist-macroexpand-region
+;;     "oR" 'alchemist-macroexpand-print-region
+;;     "o!" 'alchemist-macroexpand-close-popup))
 
 ;; (use-package dap-elixir
 ;;   :commands (dap-debug-edit-template)
--- configs/init-lsp.el
+++ configs/init-lsp.el
@@ -1,28 +1,28 @@
-(use-package lsp-mode
-  :hook (c-mode . lsp)
-  ;; :hook (elixir-mode . lsp)
-  :commands lsp
-  :init
-  ;; (add-to-list 'exec-path "~/.emacs.d/lsp/")
-  (add-to-list 'company-backends 'company-lsp)
-  (setq lsp-keymap-prefix "s-l")
-  :config
-  (lsp-enable-which-key-integration)
-  (setq lsp-file-watch-threshold nil))
+;; (use-package lsp-mode
+;;   :hook (c-mode . lsp)
+;;   ;; :hook (elixir-mode . lsp)
+;;   :commands lsp
+;;   :init
+;;   ;; (add-to-list 'exec-path "~/.emacs.d/lsp/")
+;;   (add-to-list 'company-backends 'company-lsp)
+;;   (setq lsp-keymap-prefix "s-l")
+;;   :config
+;;   (lsp-enable-which-key-integration)
+;;   (setq lsp-file-watch-threshold nil))
 
-(use-package lsp-ui
-  :hook (lsp-mode . lsp-ui-mode))
+;; (use-package lsp-ui
+;;   :hook (lsp-mode . lsp-ui-mode))
 
-(use-package company-lsp
-  :init
-  (setq company-lsp-enable-snippet t)
-  (push 'company-lsp company-backends))
+;; (use-package company-lsp
+;;   :init
+;;   (setq company-lsp-enable-snippet t)
+;;   (push 'company-lsp company-backends))
 
-(use-package lsp-ivy
-  :commands lsp-ivy-workspace-symbol)
+;; (use-package lsp-ivy
+;;   :commands lsp-ivy-workspace-symbol)
 
-(use-package lsp-treemacs
-  :commands lsp-treemacs-errors-list)
+;; (use-package lsp-treemacs
+;;   :commands lsp-treemacs-errors-list)
 
 ;; (use-package dap-mode
 ;;   :init
@@ -36,4 +36,14 @@
   ;; :ensure-system-package (tsc . "npm install -g typescript")
   ;; :ensure-system-package (typescript-language-server . "npm install -g typescript-language-server")
   ;; :ensure-system-package (vls . "npm install -g vue-language-server")
+
+(use-package jsonrpc
+  :quelpa (jsonrpc :fetcher github :repo "manateelazycat/nox"))
+
+(use-package nox
+  :quelpa (nox :fetcher github :repo "manateelazycat/nox")
+  :init
+  (add-to-list 'exec-path (concat private/config-directory "elixir-ls"))
+  (add-hook 'elixir-mode-hook 'nox-ensure))
+
 (provide 'init-lsp)
