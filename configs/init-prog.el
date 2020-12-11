053738c4e2a14e7658664e1d6bd53d901df289cc
Kenton <smartepsh@gmail.com>
2020-12-11 20:26:32 +0800

    feat: install format-all from github for latest

--- configs/init-prog.el
+++ configs/init-prog.el
@@ -1,7 +1,13 @@
+;; (use-package format-all
+;;   :hook (elixir-mode . format-all-mode)
+;;   :hook (emacs-lisp-mode . format-all-mode)
+;;   :commands format-all-buffer)
+
 (use-package format-all
+  :quelpa (format-all :fetcher github :repo "lassik/emacs-format-all-the-code")
   :hook (elixir-mode . format-all-mode)
-  :hook (emacs-lisp-mode . format-all-mode)
-  :commands format-all-buffer)
+  :hook (sql-mode . format-all-mode)
+  :hook (emacs-lisp-mode . format-all-mode))
 
 (use-package exec-path-from-shell
   :config
