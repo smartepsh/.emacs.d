f8c3e8d6b161ffeb6153afe5d0ec8b63d097de82
Kenton <smartepsh@gmail.com>
2021-06-13 14:36:16 +0800

    feat: disable old scheme config by geiser

--- configs/init-scheme.el
+++ configs/init-scheme.el
@@ -1,54 +1,62 @@
-(setq scheme-program-name "mit-scheme")
+;; (setq scheme-program-name "mit-scheme")
 
-(local-leader
-  :keymaps 'scheme-mode-map
-  "sr" 'scheme-send-region
-  "sl" '+scheme-send-line
-  "sd" 'scheme-send-definition
-  "ss" '+scheme-send-last-sexp
-  "," '+scheme-send-line
-  "r" '+run-scheme-split
-  "c" '+clear-scheme-buffer
-  )
+(use-package geiser-mit
+  :defer t
+  :commands (run-mit)
+  :hook (scheme-mode . geiser-mode)
+  :init
+  (setq geiser-active-implementations '(mit)
+        geiser-default-implementation 'mit
+        geiser-repl-use-other-window nil
+        geiser-repl-window-allow-split nil))
 
-(local-leader
-  :keymaps 'inferior-scheme-mode-map
-  "l" 'scheme-load-file
-  "c" 'comint-clear-buffer
-  "q" '+close-scheme
-  )
+;; (local-leader
+;;   :keymaps 'scheme-mode-map
+;;   "sr" 'scheme-send-region
+;;   "sl" '+scheme-send-line
+;;   "sd" 'scheme-send-definition
+;;   "ss" '+scheme-send-last-sexp
+;;   "," '+scheme-send-line
+;;   "r" '+run-scheme-split
+;;   "c" '+clear-scheme-buffer)
 
-(defun +close-scheme ()
-  (interactive)
-  (if (get-buffer-process (current-buffer))
-      (progn
-	(set-process-query-on-exit-flag (get-buffer-process (current-buffer)) nil)
-	(kill-buffer-and-window))))
+;; (local-leader
+;;   :keymaps 'inferior-scheme-mode-map
+;;   "l" 'scheme-load-file
+;;   "c" 'comint-clear-buffer
+;;   "q" '+close-scheme)
 
-(defun +run-scheme-split ()
-  (interactive)
-  (if (get-buffer "*scheme*")
-      (display-buffer "*scheme*")
-    (split-window-horizontally)
-    (other-window 1)
-    (run-scheme scheme-program-name)
-    (other-window 1)))
+;; (defun +close-scheme ()
+;;   (interactive)
+;;   (if (get-buffer-process (current-buffer))
+;;       (progn
+;; 	(set-process-query-on-exit-flag (get-buffer-process (current-buffer)) nil)
+;; 	(kill-buffer-and-window))))
 
-(defun +scheme-send-last-sexp ()
-  (interactive)
-  (right-char)
-  (scheme-send-last-sexp)
-  (backward-char)
-  )
+;; (defun +run-scheme-split ()
+;;   (interactive)
+;;   (if (get-buffer "*scheme*")
+;;       (display-buffer "*scheme*")
+;;     (split-window-horizontally)
+;;     (other-window 1)
+;;     (run-scheme scheme-program-name)
+;;     (other-window 1)))
 
-(defun +scheme-send-line ()
-  (interactive)
-  (scheme-send-region (line-beginning-position) (line-end-position)))
+;; (defun +scheme-send-last-sexp ()
+;;   (interactive)
+;;   (right-char)
+;;   (scheme-send-last-sexp)
+;;   (backward-char)
+;;   )
 
-(defun +clear-scheme-buffer ()
-  (interactive)
-  (switch-to-buffer "*scheme*")
-  (comint-clear-buffer)
-  (switch-to-prev-buffer))
+;; (defun +scheme-send-line ()
+;;   (interactive)
+;;   (scheme-send-region (line-beginning-position) (line-end-position)))
+
+;; (defun +clear-scheme-buffer ()
+;;   (interactive)
+;;   (switch-to-buffer "*scheme*")
+;;   (comint-clear-buffer)
+;;   (switch-to-prev-buffer))
 
 (provide 'init-scheme)
