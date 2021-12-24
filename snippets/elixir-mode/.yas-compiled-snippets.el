72478c53c77370df62cb2812ef2de782868133c7
Kenton Wang <smartepsh@gmail.com>
2021-12-24 22:40:05 +0800

    feat: hugo yasnippet

--- snippets/elixir-mode/.yas-compiled-snippets.el
+++ snippets/elixir-mode/.yas-compiled-snippets.el
@@ -2,7 +2,7 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'elixir-mode
-                     '(("ins" "IO.inspect(${})\n" "inspect" nil nil nil "/Users/smartepsh/.emacs.d/snippets/elixir-mode/inspect" nil nil)))
+		     '(("ins" "IO.inspect(${})\n" "inspect" nil nil nil "/Users/smartepsh/.emacs.d/snippets/elixir-mode/inspect" nil nil)))
 
 
-;;; Do not edit! File generated at Wed May  5 12:30:31 2021
+;;; Do not edit! File generated at Sat Dec 25 01:59:26 2021
--- snippets/org-mode/.yas-compiled-snippets.el
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -2,7 +2,8 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'org-mode
-                     '(("isr" "#+name: ${}\n#+begin_src R :noweb yes\n  ${}\n#+end_src\n" "r-block" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/r-block" nil nil)))
+		     '(("isr" "#+name: ${}\n#+begin_src R :noweb yes\n  ${}\n#+end_src\n" "r-block" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/r-block" nil nil)
+		       ("hugo" "#+HUGO_BASE_DIR: ~/Kenton/kenton.wang/\n#+HUGO_SECTION: posts\n#+TITLE: $1\n#+DATE: `(format-time-string \"%Y-%m-%d\")`\n#+HUGO_AUTO_SET_LASTMOD: t\n#+HUGO_TAGS: $2\n#+HUGO_CATEGORIES: $3\n#+EXPORT_FILE_NAME $4\n#+HUGO_DRAFT: false\n\n$0" "hugo" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/hugo" nil nil)))
 
 
-;;; Do not edit! File generated at Wed May  5 12:30:31 2021
+;;; Do not edit! File generated at Sat Dec 25 01:59:26 2021
