3b0b3d682b9bfd61623605702eca64f0b347a806
Kenton Wang <smartepsh@gmail.com>
2022-12-07 00:37:33 +0800

    feat: update hugo yas-snippet & set org-hugo-base-dir globally

--- snippets/elixir-mode/.yas-compiled-snippets.el
+++ snippets/elixir-mode/.yas-compiled-snippets.el
@@ -5,4 +5,4 @@
 		     '(("ins" "IO.inspect(${})\n" "inspect" nil nil nil "/Users/smartepsh/.emacs.d/snippets/elixir-mode/inspect" nil nil)))
 
 
-;;; Do not edit! File generated at Sat Dec 25 01:59:26 2021
+;;; Do not edit! File generated at Wed Dec  7 00:10:05 2022
