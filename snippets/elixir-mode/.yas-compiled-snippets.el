2871192ef7f8066b71548e599cbf5315e32ed071
Kenton Wang <smartepsh@gmail.com>
2023-02-12 21:23:31 +0800

    feat: setup mermaid

--- snippets/elixir-mode/.yas-compiled-snippets.el
+++ snippets/elixir-mode/.yas-compiled-snippets.el
@@ -5,4 +5,4 @@
 		     '(("ins" "IO.inspect(${})\n" "inspect" nil nil nil "/Users/smartepsh/.emacs.d/snippets/elixir-mode/inspect" nil nil)))
 
 
-;;; Do not edit! File generated at Wed Dec  7 00:10:05 2022
+;;; Do not edit! File generated at Wed Dec  7 23:06:24 2022
--- snippets/org-mode/.yas-compiled-snippets.el
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -3,7 +3,8 @@
 ;;;
 (yas-define-snippets 'org-mode
 		     '(("isr" "#+name: ${}\n#+begin_src R :noweb yes\n  ${}\n#+end_src\n" "r-block" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/r-block" nil nil)
-		       ("hugo" ":HUGO_BASE_DIR: ~/Kenton/kenton.wang/\n:HUGO_SECTION: $1/$2\n:TITLE: $3\n:DATE: `(format-time-string \"%Y-%m-%d\")`\n:HUGO_AUTO_SET_LASTMOD: t\n:HUGO_TAGS: [$4]\n:HUGO_CATEGORIES: $5\n:EXPORT_FILE_NAME $6\n:HUGO_DRAFT: false\n\n$0" "hugo" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/hugo" nil nil)))
+		       ("<mermaid" "#+begin_mermaid\n$0\n#+end_mermaid" "mermaid" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/mermaid" nil nil)
+		       ("hugo" ":EXPORT_HUGO_SECTION: $1/$2\n:TITLE: $3\n:EXPORT_DATE: `(format-time-string \"%Y-%m-%d\")`\n:HUGO_AUTO_SET_LASTMOD: t\n:HUGO_TAGS: [$4]\n:HUGO_CATEGORIES: $5\n:EXPORT_FILE_NAME: $6\n:HUGO_DRAFT: false\n\n$0" "hugo" nil nil nil "/Users/smartepsh/.emacs.d/snippets/org-mode/hugo" nil nil)))
 
 
-;;; Do not edit! File generated at Wed Dec  7 00:10:05 2022
+;;; Do not edit! File generated at Wed Dec  7 23:06:24 2022
