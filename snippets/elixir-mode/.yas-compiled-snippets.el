dcef93c23af43b6e41b95abc40a26c8a3eae07db
Kenton Wang <smartepsh@gmail.com>
2023-12-04 03:11:14 +0800

    feat: setup new configs with new org-roam directory

---
+++ snippets/elixir-mode/.yas-compiled-snippets.el
@@ -0,0 +1,8 @@
+;;; Compiled snippets and support files for `elixir-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'elixir-mode
+		     '(("ins" "IO.inspect(${})\n" "inspect" nil nil nil "/Users/smartepsh/.emacs.d/snippets/elixir-mode/inspect" nil nil)))
+
+
+;;; Do not edit! File generated at Wed Dec  7 23:06:24 2022
